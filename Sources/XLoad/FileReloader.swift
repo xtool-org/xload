e373a685a86e6528e3037a0fbeb4ab1f1529da02
Kabir Oberai <oberai.kabir@gmail.com>
2026-05-27 00:55:59 -0400

    Clean up

---
+++ Sources/XLoad/FileReloader.swift
@@ -0,0 +1,44 @@
+import Foundation
+import InjectionImpl
+import System
+
+actor FileReloader {
+    private var loader = Reloader()
+
+    func watch(directory: String) async throws {
+        let changes = try await watchDirectory(directory)
+        var loadedLibraries: Set<String> = []
+        for try await _ in changes {
+            let contentsArr = try FileManager.default.contentsOfDirectory(atPath: directory)
+                .filter { $0.hasSuffix(".dylib") }
+                .map { "\(directory)/\($0)" }
+            let contents = Set(contentsArr)
+            let newLibraries = contents.subtracting(loadedLibraries)
+            loadedLibraries.formUnion(newLibraries)
+            for library in newLibraries {
+                reload(library)
+            }
+        }
+    }
+
+    private func reload(_ dylib: String) {
+        guard let (image, classes) = loader.loadAndPatch(in: dylib) else {
+            print("[XLoad] Failed to load image: \(dylib)")
+            return
+        }
+        loader.sweeper.sweepAndRunTests(image: image, classes: classes)
+    }
+
+    private func watchDirectory(_ path: String) async throws -> AsyncStream<Void> {
+        let file = try FileDescriptor.open(path, .init(rawValue: O_EVTONLY))
+        let source = DispatchSource.makeFileSystemObjectSource(
+            fileDescriptor: file.rawValue,
+            eventMask: .all
+        )
+        let (stream, continuation) = AsyncStream<Void>.makeStream()
+        source.setEventHandler { continuation.yield() }
+        continuation.onTermination = { _ in source.cancel() }
+        source.resume()
+        return stream
+    }
+}
--- Sources/XLoad/XLoad.swift
+++ Sources/XLoad/XLoad.swift
@@ -1,7 +1,5 @@
-import CXLoad
-import InjectionImpl
 import Foundation
-import System
+import CXLoad
 
 @c @implementation func xload_load() {
     print("[XLoad] Loading...")
@@ -32,53 +30,6 @@ private func loadWatcher() async throws {
 
     print("[XLoad] Loading watcher...")
 
-    let changes = try await watchDirectory(watchPath)
-
     let reloader = FileReloader()
-
-    var loadedLibraries: Set<String> = []
-    for try await _ in changes {
-        let contentsArr = try FileManager.default.contentsOfDirectory(atPath: watchPath)
-            .filter { $0.hasSuffix(".dylib") }
-            .map { "\(watchPath)/\($0)" }
-        let contents = Set(contentsArr)
-        let newLibraries = contents.subtracting(loadedLibraries)
-        loadedLibraries.formUnion(newLibraries)
-        for library in newLibraries {
-            await reloader.reload(library)
-        }
-    }
-}
-
-private func watchDirectory(_ path: String) async throws -> AsyncStream<Void> {
-    let file = try FileDescriptor.open(path, .init(rawValue: O_EVTONLY))
-    let source = DispatchSource.makeFileSystemObjectSource(
-        fileDescriptor: file.rawValue,
-        eventMask: .all
-    )
-    let (stream, continuation) = AsyncStream<Void>.makeStream()
-    source.setEventHandler { continuation.yield() }
-    continuation.onTermination = { _ in source.cancel() }
-    source.resume()
-    return stream
-}
-
-actor FileReloader {
-    private var loader = Reloader()
-
-    func reload(_ dylib: String) {
-        guard let (image, classes) = loader.loadAndPatch(in: dylib) else {
-            print("[XLoad] Failed to load image: \(dylib)")
-            return
-        }
-        loader.sweeper.sweepAndRunTests(image: image, classes: classes)
-    }
-}
-
-struct StringError: Error, CustomStringConvertible {
-    var description: String
-
-    init(_ description: String) {
-        self.description = description
-    }
+    try await reloader.watch(directory: watchPath)
 }
